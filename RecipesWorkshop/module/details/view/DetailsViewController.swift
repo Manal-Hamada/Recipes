8257e6aecd86f3ac7a830177a01f1312d1203562
haidy <>
2023-05-26 16:15:25 +0300

    Page Header UI

---
+++ RecipesWorkshop/module/details/view/DetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsViewController.swift
+//  RecipesWorkshop
+//
+//  Created by JETS Mobile Lab4 on 26/05/2023.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+   
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
