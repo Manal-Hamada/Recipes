07b602f51fb10b0afc7812907abe032db3f7b8d9
haidy <>
2023-05-26 19:16:53 +0300

    ingredients and instructions sections

--- RecipesWorkshop/module/details/view/DetailsViewController.swift
+++ RecipesWorkshop/module/details/view/DetailsViewController.swift
@@ -7,16 +7,43 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
-   
+class DetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-
+    @IBOutlet weak var ingredientsTable: UITableView!
+    @IBOutlet weak var instructionsTable: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if tableView == ingredientsTable{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ingredientsCell")
+            cell?.textLabel!.text = "Haidy"
+            return cell!
+        }
+        
+        if tableView == instructionsTable{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "instructionsCell")
+            cell?.textLabel?.text = "Muhammed"
+            return cell!
+        }
+        
+        
+        return UITableViewCell()
+    }
+    
+   
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-
 }
+
